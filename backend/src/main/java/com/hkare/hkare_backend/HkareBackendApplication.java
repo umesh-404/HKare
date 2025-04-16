5a8005c6f9857c9d14716cfe40e063fed9dacc2c
Umesh J <umeshbtech05@gmail.com>
2025-04-16 22:36:19 +0530

    added backend folder

---
+++ backend/src/main/java/com/hkare/hkare_backend/HkareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hkare.hkare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkareBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hkare/hkare_backend/HkareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkare.hkare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
